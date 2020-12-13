42a5c4bcdc6b75b736c1718503165b486b101f23
Sandeep <sami0816@netcracker.com>
2020-12-13 21:40:04 +0530

    changes made to Jenkinsfile

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -49,8 +49,63 @@ terraform {
   }
 }
 
-module "vpc" {
-  source = module/vpc
+resource "aws_vpc" "vpc" {
+  cidr_block       = var.vpc
+  instance_tenancy = "default"
+
+  tags = {
+    "Name" = "eks_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+  "Name" = "eks_int_gw"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  for_each   = local.az-subnet
+  vpc_id     = aws_vpc.vpc.id
+  availability_zone = each.value.az
+  cidr_block = each.value.private_love-bonito_cidr
+  map_public_ip_on_launch = true
+
+  depends_on = [aws_internet_gateway.gw]
+
+  tags = {
+    "Name" = "eks_subnets"
+    "kubernetes.io/cluster/${var.eks-cluster-name}" = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/${var.eks-cluster-name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }  
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+      "Name" = "eks_route"
+  }
+}
+
+resource "aws_route_table_association" "route_link" {
+  count = 2
+
+  subnet_id      = aws_subnet.subnet.*.id[count.index]
+  route_table_id = aws_route_table.route.id
+
+  tags = {
+      "Name" = "eks_route_link"
+  }
 }
 
 provider "kubernetes" {
@@ -67,7 +122,7 @@ module "love-bonito-k8cluster" {
   cluster_name    = var.eks-cluster-name
   cluster_version = "1.18"
   subnets         = tolist(data.aws_subnet_ids.subnet_id.ids)
-  vpc_id          = module.vpc.id
+  vpc_id          = aws_vpc.vpc.id
 
   node_groups = [
     {
@@ -84,10 +139,10 @@ module "love-bonito-k8cluster" {
 
 
 data "aws_subnet_ids" "subnet_id" {
-  vpc_id = module.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   depends_on = [ 
-    module.vpc.subnet
+    aws_subnet.subnet
    ]
 }
 
