de9913296eabd89fa9f935b1bb6031538a1d5d43
Sandeep <sami0816@netcracker.com>
2020-12-13 16:09:44 +0530

    changes made to Jenkinsfile

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -89,9 +89,8 @@ module "love-bonito-k8cluster" {
 
   node_groups = [
     {
-      instance_type    = "t2.micro"
       max_capacity     = 2
-      desired_capacity = 2
+      desired_capacity = 1
       min_capacity     = 1
     }
   ]
